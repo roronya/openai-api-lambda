f60ad766eb3381c3b548e2eeaecc3f8d35eba6db
roronya <roronya628@gmail.com>
2023-10-08 19:11:18 +0900

    modify if

--- forward_to_openai.py
+++ forward_to_openai.py
@@ -1,6 +1,5 @@
 import logging
 import os
-import json
 import openai
 
 logger = logging.getLogger()
@@ -10,11 +9,11 @@ openai.api_key = os.environ['OPENAI_API_KEY']
 
 def lambda_handler(event, context):
     logger.info(event)
-    # bodyに文字列としてjsonが入っていることを期待している
-    messages = json.loads(event["body"])
+    messages = event["body"]
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=messages
     )
     logger.info(response)
     return response
+
