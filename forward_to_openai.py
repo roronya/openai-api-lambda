ac388cdefd75ca8e226c6589fe3727b1753e879c
roronya <roronya628@gmail.com>
2023-10-08 15:04:52 +0900

    動作確認できた

--- forward_to_openai.py
+++ forward_to_openai.py
@@ -6,28 +6,40 @@ import requests
 
 OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
 OPENAI_ENDPOINT = "https://api.openai.com/v1/engines/davinci/completions"
+HEADERS = {
+    "Authorization": f"Bearer {os.environ['OPENAI_API_KEY']}",
+    "Content-Type": "application/json"
+}
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
 def lambda_handler(event, context):
-    forward(event)
+    return forward(event)
 
 
 def forward(event):
-    headers = {
-        "Authorization": f"Bearer {OPENAI_API_KEY}",
-        "Content-Type": "application/json",
+    # API Gatewayのプロキシ統合から本文を取得
+    body = json.loads(event["body"])
+
+    # OpenAI APIに転送するためのペイロードを作成
+    data = {
+        "prompt": body["prompt"],
+        "max_tokens": body["max_tokens"]
     }
 
-    req = json.dumps(event)
-    logger.info(req)
-    response = requests.post(OPENAI_ENDPOINT, headers=headers, data=req)
+    logger.info(event)
+    logger.info(data)
+    response = requests.post(OPENAI_ENDPOINT, headers=HEADERS, json=data)
+    logger.info(response)
     result = response.json()
     logger.info(result)
 
-    return result
+    return {
+        "statusCode": response.status_code,
+        "body": json.dumps(result)
+    }
 
 
 # for local debugging
--- main.tf
+++ main.tf
@@ -121,3 +121,69 @@ resource "aws_iam_role_policy_attachment" "lambda_logging_attach" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = aws_iam_policy.lambda_logging.arn
 }
+
+# api gateway logging
+resource "aws_api_gateway_account" "api_gw_account" {
+  cloudwatch_role_arn = aws_iam_role.api_gw_logging_role.arn
+}
+
+resource "aws_iam_role" "api_gw_logging_role" {
+  name = "APIGatewayLoggingRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Principal = {
+        Service = "apigateway.amazonaws.com"
+      },
+      Effect = "Allow",
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "api_gw_logging_policy" {
+  name = "APIGatewayLoggingPolicy"
+  role = aws_iam_role.api_gw_logging_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:DescribeLogGroups",
+        "logs:DescribeLogStreams",
+        "logs:PutLogEvents",
+        "logs:GetLogEvents",
+        "logs:FilterLogEvents"
+      ],
+      Effect   = "Allow",
+      Resource = "*"
+    }]
+  })
+}
+
+resource "aws_api_gateway_stage" "prod" {
+  deployment_id = aws_api_gateway_deployment.openai_deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.openai_api.id
+  stage_name    = "prod"
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api_gw_logs.arn
+    format          = "$context.identity.sourceIp - - [$context.requestTime] \"$context.httpMethod $context.resourcePath $context.protocol\" $context.status $context.responseLength $context.requestId"
+  }
+}
+
+resource "aws_cloudwatch_log_group" "api_gw_logs" {
+  name = "/aws/apigateway/OpenAI_Forwarder"
+}
+
+resource "aws_lambda_permission" "apigw_lambda_permission" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.openai_forwarder.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_deployment.openai_deployment.execution_arn}/*/*"
+}
