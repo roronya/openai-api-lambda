322b158574f6bc610606e16ad7f8319e742c403f
roronya <roronya628@gmail.com>
2023-10-08 13:09:54 +0900

    complete lambda deploy

--- forward_to_openai.py
+++ forward_to_openai.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import json
 import boto3
@@ -6,13 +7,30 @@ import requests
 OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
 OPENAI_ENDPOINT = "https://api.openai.com/v1/engines/davinci/completions"
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 
 def lambda_handler(event, context):
+    forward(event)
+
+
+def forward(event):
     headers = {
         "Authorization": f"Bearer {OPENAI_API_KEY}",
         "Content-Type": "application/json",
     }
 
-    response = requests.post(OPENAI_ENDPOINT, headers=headers, data=json.dumps(event))
+    req = json.dumps(event)
+    logger.info(req)
+    response = requests.post(OPENAI_ENDPOINT, headers=headers, data=req)
+    result = response.json()
+    logger.info(result)
 
-    return response.json()
+    return result
+
+
+# for local debugging
+if __name__ == '__main__':
+    event = {"prompt": "Translate the following English text to Japanese: Hello World", "max_tokens": 50}
+    print(forward(event))
